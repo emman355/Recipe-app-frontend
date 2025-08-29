d6b6877f13afe2e9578a37e74553c6e0848b4972
emman355 <earevalo355@gmail.com>
2025-08-29 15:34:14 +0800

    fix: fix package dependencies

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,10 +2,10 @@ import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Slot } from "expo-router";
 import Toast from "react-native-toast-message";
-import CustomToast from "@/components/CustomToast";
-import SafeScreen from "@/components/SafeScreen";
+import CustomToast from "../components/CustomToast";
+import SafeScreen from "../components/SafeScreen";
 import { Provider } from "react-redux";
-import { store } from "@/store/";
+import { store } from "../store";
 
 export default function RootLayout() {
   return (
